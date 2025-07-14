3ec68f5e16e269e6bfa7a8f2dd5c0a5b1e77a4cb
zqw <230054070338@qq.com>
2025-07-14 17:58:58 +0800

    hot-fix commit

--- git-test01/src/main/java/com/atguigu/git.java
+++ git-test01/src/main/java/com/atguigu/git.java
@@ -1,7 +1,7 @@
 package com.atguigu;
 
 public class git {
-    public static void main(String[] args) {
+    public static void main(String[] args) {   // 绿色代表分支，黄色代表hard指针
         System.out.println("hello git!");
         System.out.println("hello git2!");
         System.out.println("hello git3!");     // 蓝色文件可以直接提交本地库，不用添加暂停区
